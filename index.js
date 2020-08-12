971c455c3c934ef9bc4fdb433f476176a1a3ecb6
nikintan <intanzura@yahoo.com>
2020-08-13 00:33:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+router.get('/',(req, res)=>{
+	res.json({ message: 'hooray! welcome to our API!'});
+});
+
+router.get('/timestamp/:date',(req, res)=>{
+	if(parseInt(req.params.date*1000)){
+	res.json({"unix":req.params.date, "utc":new Date(req.params.date*1000)});
+   }else {
+	if (new Date(req.params.date) !="Invalid Date"){
+		res.json({"unix":req.params.date, "utc":(new Date(req.params.date)).getTime/1000});
+	}
+	else {
+		res.json({"error" : "Invalid Date"})
+		}
+   	}
+   });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happen on port ' + port);
